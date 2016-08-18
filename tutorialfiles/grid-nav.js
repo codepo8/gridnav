395758e83087dd45f6bf326aa39f591af614efe4
Christian Heilmann <chris.heilmann@gmail.com>
2016-08-18 15:04:12 +0100

    cleanup

--- tutorialfiles/grid-nav.js
+++ tutorialfiles/grid-nav.js
@@ -1,24 +1,29 @@
 (function(){
   var list = document.querySelector('ul');
   var items = list.querySelectorAll('button');
-  var listwidth = list.offsetWidth;
-  var itemwidth = list.firstElementChild.offsetWidth;
-  var amount = Math.floor(listwidth / itemwidth);
-  var codes = {38:-amount,40:amount,39:1,37:-1};
+  var amount = Math.floor(
+        list.offsetWidth / 
+        list.firstElementChild.offsetWidth
+      );
+  var codes = {
+    38: -amount,
+    40: amount, 
+    39: 1,
+    37: -1
+  };
   for (var i = 0; i < items.length; i++) {
-    items[i].count = i;
+    items[i].index = i;
   }
   function handlekeys(ev) {
     var keycode = ev.keyCode;
     if (codes[keycode]) {
       var t = ev.target;
-      if (t.count) {
-        if (items[t.count + codes[keycode]]) {
-          items[t.count + codes[keycode]].focus();
+      if (t.index !== undefined) {
+        if (items[t.index + codes[keycode]]) {
+          items[t.index + codes[keycode]].focus();
         }
       }
     }
   }
-
   list.addEventListener('keyup', handlekeys);
 })();
